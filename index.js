a909e0e81665b534e38da56c279a68f9d41eeba2
meinjam <injam.cse@gmail.com>
2023-03-13 13:26:01 +0600

    done

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
 
 let notes = [
   {
@@ -27,7 +31,45 @@ app.get('/api/notes', (request, response) => {
   response.json(notes);
 });
 
-const PORT = 3001;
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === parseInt(id));
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: Date.now(),
+  };
+
+  console.log(note);
+  notes = notes.concat(note);
+  response.json(note);
+});
+
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
