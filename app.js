4b436326a401486a1e31dd07e9a828092a4d2c28
meinjam <injam.cse@gmail.com>
2023-03-14 11:48:37 +0600

    user get and add done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const notesRouter = require('./controllers/notes');
+const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -30,6 +31,7 @@ app.get('/', (request, response) => {
 });
 
 app.use('/api/notes', notesRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
