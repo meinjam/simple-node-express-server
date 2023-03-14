606f112eb1d00f6da62188778c26bf4a361e090d
meinjam <injam.cse@gmail.com>
2023-03-14 12:11:02 +0600

    users note get done

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
+const User = require('../models/user');
 
 notesRouter.get('/', (request, response) => {
   Note.find({}).then((notes) => {
@@ -7,8 +8,10 @@ notesRouter.get('/', (request, response) => {
   });
 });
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
   const body = request.body;
+
+  const user = await User.findById(body.userId);
 
   if (!body.content) {
     return response.status(400).json({
@@ -19,14 +22,20 @@ notesRouter.post('/', (request, response, next) => {
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id,
   });
 
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote);
-    })
-    .catch((error) => next(error));
+  const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+  response.json(savedNote);
+
+  // note
+  //   .save()
+  //   .then((savedNote) => {
+  //     response.json(savedNote);
+  //   })
+  //   .catch((error) => next(error));
 });
 
 notesRouter.get('/:id', (request, response, next) => {
